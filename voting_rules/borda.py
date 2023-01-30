63b54e68a0d0dd9d59ec6f7f8d3729593b8814b6
Rafik Hadfi <rafik.hadfi@gmail.com>
2023-01-30 16:53:04 +0900

    Update borda.py

--- voting_rules/borda.py
+++ voting_rules/borda.py
@@ -5,15 +5,10 @@ from typing import Callable, Set
 
 def create_rule(profile: Set[int]) -> Callable[[int], int]:
     def borda_rule(candidate: int) -> int:
-        """
-        Parameters: candidate (base candidate for scoring)
-        """
         # Max score to be applied with borda count
         top_score = len(profile.candidates) - 1
-
         # Get pairwise scores
         scores = [n_votes * (top_score - ballot.index(candidate)) for n_votes, ballot in profile.pairs]
-
         # Return the total score
         return sum(scores)
     borda_rule.__name__ = 'borda'
