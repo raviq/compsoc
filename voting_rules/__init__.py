6de9e5e6617c60d3e3e1f27f2808e0646d3c3815
asoderlind <axesode@gmail.com>
2023-03-11 13:39:25 +0900

    move

--- voting_rules/__init__.py
+++ voting_rules/__init__.py
@@ -1,7 +1,10 @@
-
+"""
+init
+"""
 import glob
 import os
 from os.path import dirname, basename, isfile, join
 
-modules = [f for f in os.listdir(os.path.dirname(__file__)) if f.endswith('.py') and f != '__init__.py']
+modules = [f for f in os.listdir(os.path.dirname(__file__)) if
+           f.endswith('.py') and f != '__init__.py']
 __all__ = [os.path.basename(f)[:-3] for f in modules]
