5649b4d7b79cf95a9023d5e0795890b1a771e97e
asoderlind <axesode@gmail.com>
2023-03-13 19:00:00 +0900

    remove ambiguous exception-catching, sort imports

--- run.py
+++ run.py
@@ -4,17 +4,17 @@ Entry point of the voting tournament.
 Rafik Hadfi <rafik.hadfi@gmail.com>
 """
 
-import re
 import argparse
 import importlib
 import inspect
-from tqdm import trange
+import re
 from typing import List, Tuple
-from profile import Profile
-from compsoc.utils import plot_final_results
-from compsoc.voter_model import generate_multinomial_dirichlet_votes, generate_random_votes, \
-    generate_gaussian_votes
 
+from tqdm import trange
+
+from compsoc.profile import Profile
+from compsoc.utils import plot_final_results
+from compsoc.voter_model import generate_gaussian_votes, generate_multinomial_dirichlet_votes, generate_random_votes
 from compsoc.voting_rules.borda import borda_rule
 from compsoc.voting_rules.borda_gamma import get_borda_gamma
 from compsoc.voting_rules.copeland import copeland_rule
@@ -99,41 +99,38 @@ def evaluate_voting_rules(num_candidates, num_voters, topn, voters_model):
 
 
 def main():
-    try:
-        # Import voter models names from models.py.
-        # Each must be implemented as 'generate_M_votes'
-        voter_model_folder = "voter_model"
-        module = importlib.import_module(voter_model_folder)
-        functions = inspect.getmembers(module, inspect.isfunction)
-        voters_model_distributions = []
-        for function in functions:
-            match = re.search(r"generate_(.*)_votes", function[0])
-            if match:
-                voters_model_distributions.append(match.group(1))
-        if not voters_model_distributions:
-            raise Exception(f"No voter models found in {voter_model_folder}.")
-        # Loading arguments
-        parser = argparse.ArgumentParser()
-        parser.add_argument("num_candidates", type=int, help="Number of candidates")
-        parser.add_argument("num_voters", type=int, help="Number of voters")
-        parser.add_argument("num_iterations", type=int, help="Number of iterations")
-        parser.add_argument("num_topn", type=int, help="Top N.")
-        parser.add_argument("voters_model", type=str,
-                            choices=voters_model_distributions,
-                            help=f"Model for the generation of voters: "
-                                 f"{', '.join(voters_model_distributions)}")
-        parser.add_argument("-v", "--verbose", action="store_true",
-                            help="Increases output verbosity")
-        args = parser.parse_args()
-        # Results
-        results = {}
-        for i in trange(args.num_iterations):
-            results[i] = evaluate_voting_rules(args.num_candidates, args.num_voters, args.num_topn,
-                                               args.voters_model)
-        plot_final_results(args.voters_model, results, args.num_voters, args.num_candidates,
-                           args.num_topn, args.num_iterations)
-    except Exception as e:
-        print(f"Error: {e}")
+    # Import voter models names from models.py.
+    # Each must be implemented as 'generate_M_votes'
+    voter_model_folder = "compsoc.voter_model"
+    module = importlib.import_module(voter_model_folder)
+    functions = inspect.getmembers(module, inspect.isfunction)
+    voters_model_distributions = []
+    for function in functions:
+        match = re.search(r"generate_(.*)_votes", function[0])
+        if match:
+            voters_model_distributions.append(match.group(1))
+    if not voters_model_distributions:
+        raise Exception(f"No voter models found in {voter_model_folder}.")
+    # Loading arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num_candidates", type=int, help="Number of candidates")
+    parser.add_argument("num_voters", type=int, help="Number of voters")
+    parser.add_argument("num_iterations", type=int, help="Number of iterations")
+    parser.add_argument("num_topn", type=int, help="Top N.")
+    parser.add_argument("voters_model", type=str,
+                        choices=voters_model_distributions,
+                        help=f"Model for the generation of voters: "
+                             f"{', '.join(voters_model_distributions)}")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="Increases output verbosity")
+    args = parser.parse_args()
+    # Results
+    results = {}
+    for i in trange(args.num_iterations):
+        results[i] = evaluate_voting_rules(args.num_candidates, args.num_voters, args.num_topn,
+                                           args.voters_model)
+    plot_final_results(args.voters_model, results, args.num_voters, args.num_candidates,
+                       args.num_topn, args.num_iterations)
 
 
 if __name__ == "__main__":
